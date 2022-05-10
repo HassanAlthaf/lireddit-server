34b8412abec22ed5c74d6ec4748c5d8ded42b48d
Hassan Althaf <social@hassanalthaf.com>
2022-05-10 21:07:15 +0530

    feat(User): Login Mutation

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -1,6 +1,14 @@
 import { User } from "./../entities/User";
 import { MyContext } from "../types";
-import { Arg, Ctx, Field, InputType, Mutation, Resolver } from "type-graphql";
+import {
+  Arg,
+  Ctx,
+  Field,
+  InputType,
+  Mutation,
+  ObjectType,
+  Resolver,
+} from "type-graphql";
 
 const argon2 = require("argon2");
 
@@ -12,13 +20,31 @@ class UsernamePasswordInput {
   password: string;
 }
 
+@ObjectType()
+class FieldError {
+  @Field()
+  field: string;
+
+  @Field()
+  message: string;
+}
+
+@ObjectType()
+class UserResponse {
+  @Field(() => [FieldError], { nullable: true })
+  errors?: FieldError[];
+
+  @Field(() => User, { nullable: true })
+  user?: User;
+}
+
 @Resolver()
 export class UserResolver {
   @Mutation(() => User)
   async register(
     @Arg("options") options: UsernamePasswordInput,
     @Ctx() { em }: MyContext
-  ) {
+  ): Promise<User> {
     const hashedPassword = await argon2.hash(options.password);
 
     const user = em.create(User, {
@@ -29,5 +55,31 @@ export class UserResolver {
     await em.persistAndFlush(user);
 
     return user;
+  }
+
+  @Mutation(() => UserResponse)
+  async login(
+    @Arg("options") options: UsernamePasswordInput,
+    @Ctx() { em }: MyContext
+  ): Promise<UserResponse> {
+    const user = await em.findOne(User, { username: options.username });
+
+    if (!user) {
+      return {
+        errors: [{ field: "username", message: "Username does not exist." }],
+      };
+    }
+
+    const valid = await argon2.verify(user.password, options.password);
+
+    if (!valid) {
+      return {
+        errors: [
+          { field: "password", message: "Incorrect password provided." },
+        ],
+      };
+    }
+
+    return { user };
   }
 }
