0fb08011a6b2c82bec555731f918208385f35db5
Hassan Althaf <social@hassanalthaf.com>
2022-05-10 20:17:18 +0530

    chore: Apollo GraphQL & Express Install

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,31 @@
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
 import microConfig from "./mikro-orm.config";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
 
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
-
   await orm.getMigrator().up();
+
+  const app = express();
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver],
+      validate: false,
+    }),
+  });
+
+  await apolloServer.start();
+
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(4000, () => {
+    console.info("Server started on port 4000.");
+  });
 };
 
 main().catch((err) => {
