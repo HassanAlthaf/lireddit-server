2381260840754c15a0a0efe0edd83052b301d3b7
Hassan Althaf <social@hassanalthaf.com>
2022-05-10 20:43:20 +0530

    feat(Post): CRUD Queries and Mutations

--- src/entities/Post.ts
+++ src/entities/Post.ts
@@ -1,16 +1,23 @@
 import { Entity, PrimaryKey, Property } from "@mikro-orm/core";
+import { Field, ObjectType } from "type-graphql";
 
+@ObjectType()
 @Entity()
 export class Post {
+  @Field()
   @PrimaryKey()
   id!: number;
 
+  // Commenting this line out will hide the property from GraphQL Query Engine
+  @Field()
   @Property({ type: "text" })
   title!: string;
 
+  @Field(() => String)
   @Property({ type: "date" })
   createdAt = new Date();
 
+  @Field(() => String)
   @Property({ type: "date", onUpdate: () => new Date() })
   updatedAt = new Date();
 }
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import "reflect-metadata";
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
 import microConfig from "./mikro-orm.config";
@@ -5,6 +6,7 @@ import express from "express";
 import { ApolloServer } from "apollo-server-express";
 import { buildSchema } from "type-graphql";
 import { HelloResolver } from "./resolvers/hello";
+import { PostResolver } from "./resolvers/post";
 
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
@@ -14,8 +16,12 @@ const main = async () => {
 
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
-      resolvers: [HelloResolver],
+      resolvers: [HelloResolver, PostResolver],
       validate: false,
+    }),
+    context: () => ({
+      // Inject parameters to the resolver's context.
+      em: orm.em,
     }),
   });
 
