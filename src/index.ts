389823531786cced0e1b1a807220053c8a38d62a
Hassan Althaf <social@hassanalthaf>
2022-05-14 21:28:39 +0530

    feat(Session): Configured Sessions and Redis Client

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,10 @@ import { buildSchema } from "type-graphql";
 import { HelloResolver } from "./resolvers/hello";
 import { PostResolver } from "./resolvers/post";
 import { UserResolver } from "./resolvers/user";
+import Redis from "ioredis";
+import session from "express-session";
+import connectRedis from "connect-redis";
+import { MyContext } from "./types";
 
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
@@ -15,14 +19,38 @@ const main = async () => {
 
   const app = express();
 
+  const redisStore = connectRedis(session);
+  const redisClient = new Redis();
+
+  app.use(
+    session({
+      name: "qid",
+      store: new redisStore({
+        client: redisClient,
+        disableTTL: true, // Disable TTL of Data, Data lives infinitely.
+        disableTouch: true, // Polls to renew TTL of Data, so disable it since data must last infinitely.
+      }),
+      cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 365 * 10, // 10 years.
+        httpOnly: true,
+        secure: __prod__, // cookie only works in https
+        sameSite: "lax", // CSRF
+      },
+      secret: "some special secret",
+      resave: false,
+    })
+  );
+
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
       resolvers: [HelloResolver, PostResolver, UserResolver],
       validate: false,
     }),
-    context: () => ({
+    context: ({ req, res }): MyContext => ({
       // Inject parameters to the resolver's context.
       em: orm.em,
+      req,
+      res,
     }),
   });
 
